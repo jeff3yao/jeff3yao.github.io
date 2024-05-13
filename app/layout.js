4a4635db9846b7ac063bea7c89d8fac17eed11aa
Jeffrey Yao <j3ycodes@gmail.com>
2024-05-14 02:49:18 +0800

    first commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Jeffrey Yao",
+  description: "This is a test website made by Jeffrey",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
